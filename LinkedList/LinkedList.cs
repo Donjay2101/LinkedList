52f135e3bc24777a9badf2417265eb5afac21f54
Dony jay <deep.21jan@live.com>
2019-05-17 15:42:42 +0800

    intial commit

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace LinkedList
+{
+
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class LinkedList
+    {
+        Node _head = null;
+        Node newNode = null;
+        public void Insert(int value)
+        {
+            if(_head == null)
+            {
+                _head = new Node();
+                _head.Data = value;
+                _head.Next = null;
+            }
+            else
+            {
+                newNode = new Node();
+                newNode.Data = value;
+                newNode.Next = _head;
+                _head = newNode;
+            }
+        }
+
+
+        public Node Traverse( bool isSearch =false, int search_value =0) 
+        {
+            var _nextNode = _head;
+            int count = 1;
+            while(_nextNode !=null)
+            {
+                if(isSearch)
+                {
+                    if(_nextNode.Data == search_value)
+                    {
+                        Console.WriteLine($"value found in linked list at node {count}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.Write($"{_nextNode.Data} ");
+                    _nextNode = _nextNode.Next;
+                }
+                count++;
+                
+            }
+            return _nextNode;
+        }
+
+
+        public void DeleteStart()
+        {
+            _head = _head.Next; 
+        }
+
+
+        public void DeleteEnd()
+        {
+            var _next = new Node();
+            var _secondLast = new Node();
+            _next = _head;
+            
+            while (_next.Next != null)
+            {
+                _secondLast = _next;
+                _next = _next.Next;
+            }
+            _secondLast.Next = null;
+        }
+
+        public void Instructions()
+        {
+            Console.WriteLine("\n------------------- welcome to linkedlist program--------------------\n");
+            Console.WriteLine("Press 0 to exit");
+            Console.WriteLine("Press 1 to insert data in linked list");
+            Console.WriteLine("Press 2 to Iterate through linked list");
+            Console.WriteLine("Press 3 to search data in linked list");
+            Console.WriteLine("Press 4 to delete data in linked list from start");
+            Console.WriteLine("Press 5 to delete data in linked list from last");
+            Console.WriteLine("Press 6 to delete data in linked list from any point");
+        }
+
+        public void InovkeChoice(int choice)
+        {
+            switch(choice)
+            {
+                case 1:
+                    int valu;
+                    Console.WriteLine("Enter value to put in linked list");
+                    valu = Convert.ToInt32(Console.ReadLine());
+                    Insert(valu);
+                    break;
+                case 2:
+                    Traverse();
+                    break;
+                case 3:
+                    Console.WriteLine("enter value which you want to search");
+                    var value = Convert.ToInt32(Console.ReadLine());
+                    Traverse(true,value);
+                    break;
+                case 4:
+                    DeleteStart();
+                    Console.WriteLine("node has been deleted from start.");
+                    Traverse();
+                    break;
+                case 5:
+                    DeleteEnd();
+                    Console.WriteLine("node has been deleted from start.");
+                    Traverse();
+                    break;
+                default:
+                    Console.WriteLine("not a valid choice");
+                    break;
+            }
+            Console.WriteLine("\n");
+        }
+
+
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+
+        static readonly LinkedList _list = new LinkedList();
+
+        static void Main(string[] args)
+        {
+
+            int choice;
+            do
+            {
+                _list.Instructions();
+                choice = Convert.ToInt32(Console.ReadLine());
+                _list.InovkeChoice(choice);
+            } while (choice != 0);
+            
+        }
+    }
+}
